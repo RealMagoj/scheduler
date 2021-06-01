b2f7d4ad6d28bd4503de7fe2377841d1988fd387
RealMagoj <domgolac@hotmail.com>
2021-06-01 01:42:36 -0400

    add classnames module and update classes for DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,19 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayClass = classNames(
+    "day-list__item",
+    {
+      "day-list__item--selected": props.selected,
+      "day-list__item--full": props.spots === 0
+    }
+  );
+
   return (
     <li
+      className={dayClass}
       selected={props.selected}
       onClick={() => {props.setDay(props.name)}}
     >
