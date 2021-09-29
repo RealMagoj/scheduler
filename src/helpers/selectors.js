597a99f8bdd0ffbf12cb9295a04a0e3a73eb7312
RealMagoj <domgolac@hotmail.com>
2021-09-29 07:44:21 +0000

    update selectors.js functions to use Array.find method where applicable

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,8 @@
 export function getAppointmentsForDay(state, day) {
   const appointmentsArray = [];
-  const selectedDay = state.days.filter(stateDay => {
+  const selectedDay = state.days.find(stateDay => {
     return stateDay.name === day;
-  })[0];
+  });
   selectedDay && selectedDay.appointments.map(appointment => {
     return appointmentsArray.push(state.appointments[appointment]);
   });
@@ -11,9 +11,9 @@ export function getAppointmentsForDay(state, day) {
 
 export function getInterviewersForDay(state, day) {
   const interviewersArray = [];
-  const selectedDay = state.days.filter(stateDay => {
+  const selectedDay = state.days.find(stateDay => {
     return stateDay.name === day;
-  })[0];
+  });
   selectedDay && selectedDay.interviewers.map(interviewer => {
     return interviewersArray.push(state.interviewers[interviewer]);
   });
